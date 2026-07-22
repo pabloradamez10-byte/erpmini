dce2c353f1404b47bc040aad520205bfcdf202df
pabloradamez10-byte <pabloradamez10@gmail.com>
2026-07-21 21:55:33 -0300

    Ativa isolamento local antes de iniciar o ERP

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { installStorageIsolation } from "./utils/installStorageIsolation.js";
+
+installStorageIsolation();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
